4c338faccbcfb109c7cf335e5ee8e50b6fd9644a
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-08-20 21:28:59 +0530

    List Appropriation Started

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1506,14 +1506,42 @@ const changeListStyle = (width) => {
         $("#toolbar").removeClass("toolbar")
         $("#toolbar").first().addClass("list-group")
         $("#listOfWeather").children().addClass("list-group-item")
-    }
-    else{
+    } else {
         $("#toolbar").addClass("toolbar")
         $("#toolbar").first().removeClass("list-group")
         $("#listOfWeather").children().removeClass("list-group-item")
     }
 }
+const updateIcon = (time, iconCode, target,ifDateText) => {
+    let timeNow = time.replace(/[- :]/g, " ").charAt(16) + time.replace(/[- :]/g, " ").charAt(17);
 
+    let iconPrefix = (timeNow > 05 && timeNow < 18) ? "day" : "night";
+    $(target).addClass(`wi wi-owm-${iconPrefix}-${iconCode}`)
+
+}
+const addSuffixToDay = (dateText) => {
+    switch (dateText) {
+        case "Mon":
+        case "Fri":
+        case "Sun":
+            dateText = dateText + "day"
+            break
+        case "Tue":
+            dateText = dateText + "sday"
+            break
+        case "Wed":
+            dateText = dateText + "nesday"
+            break
+        case "Thu":
+            dateText = dateText + "rsday"
+            break
+        case "Sat":
+            dateText = dateText + "urday"
+            break
+    }
+    return dateText
+
+}
 const SearchWeather = (cityName) => {
     $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=725c271cefbc3221ab205ee4ecaaefaa`, (result) => {
 
@@ -1529,7 +1557,7 @@ const SearchWeather = (cityName) => {
         /*
                 let sunriseTime=new Date(sunrise*1000)
                 let sunsetTime=new Date(sunset*1000) Convert To GMT by toUTCSTRING */
-        let regex = /[ ]/g
+
         $("#Visibility").text(visibility / 1000 + " km")
     })
 }
@@ -1570,20 +1598,32 @@ const SearchWeatherForecast = (cityName) => {
         }
 
         showList.forEach(element => {
-            let nameOfWeekDay = "Monday";
-            let temp_min = element.main.temp_min;
-            let temp_max = element.main.temp_max;
-            $("#listOfWeather").append("<li class=pl-2>" + "<div class=card_in_li >" + "<div class=card-body>" + "<h5 class=card-title>" + nameOfWeekDay + "</h5>" + "<h6 class=card-subtitle_in_li >" + `<i class=weather_in_cards>` + "</i>" + "</h6>" + "<h6 class=card-text>" + "<span class=left-span>" + "<h6>" + temp_min + "</h6>" + "<h6>" + temp_max + "</h6>" + "</span>" + "</h6>" + "<a class=btn_in_cards >" + "Learn More" + "</a>" + "</div>" + "</div>" + "</li>")
+
+
+            let temp = element.main.temp;
+            let dateText = new Date(element.dt * 1000).toString()
+
+            let time = dateText.substring(16, 25) + "IST"
+
+            let nameOfWeekDay = addSuffixToDay(dateText.substring(0, 3))
+            let iconCode = null
+            element.weather.forEach(id => {
+                iconCode = id.id
+            })
+            console.log(iconCode)
+            let icon = updateIcon(dateText, iconCode, ".weather_in_cards")
+
+            $("#listOfWeather").append("<li class=pl-4>" + "<div class=card_in_li >" + "<div class=card-body>" + "<h5 class=card-title>" + nameOfWeekDay + "</h5>" + "<h6 class=card-subtitle_in_li >" + `<i class=weather_in_cards>` + "</i>" + "</h6>" + "<h6 class=card-text>" + "<span class=left-span>" + "<h6>" + time + "</h6>" + "<h6>" + temp + "</h6>" + "</span>" + "</h6>" + "<a class=btn_in_cards >" + "Learn More" + "</a>" + "</div>" + "</div>" + "</li>")
             $(".card_in_li").addClass("card bg-transparent border-left border-right")
             $(".card-subtitle_in_li").addClass("card-subtitle mb-2")
-            $(".weather_in_cards").addClass("wi wi-sunrise")
+
             $(".btn_in_cards").addClass("btn btn-sm btn-dark")
 
         })
     })
 }
 module.exports = $(document).ready(() => {
-    $(window).resize(()=>{
+    $(window).resize(() => {
         changeListStyle(document.body.clientWidth);
 
     })
@@ -1609,8 +1649,6 @@ module.exports = $(document).ready(() => {
 
 
 })
-
-document.querySelector("#cityValue").setAttribute("type", "text");
 },{"axios":1}],29:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
